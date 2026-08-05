72c4aa7cb0390ae3284695a8928be67cb2ca57a7
omkardeshmukh038-arch <omkardeshmukh038@gmail.com>
2026-08-05 19:46:57 +0530

    Time: 2 ms (38.16%) | Memory: 8.6 MB (54.87%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int revNum = 0;
+
+        while(n != 0){
+            int dig = 0;
+            if(revNum > INT_MAX/10 || revNum < INT_MIN/10){
+                return 0;
+            }
+            dig = n % 10;
+            revNum = revNum * 10 + dig;
+            n /= 10;
+        }
+        return revNum;
+        
+    }
+};
