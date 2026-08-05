12cd981e7363caa064fddda35dc4559fa22ff295
omkardeshmukh038-arch <omkardeshmukh038@gmail.com>
2026-08-05 19:46:36 +0530

    Time: 3 ms (66.24%) | Memory: 15.2 MB (9.52%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m ;
+        for(int i=0; i<nums.size(); i++){
+
+            int compliment = target - nums[i];
+
+            if(m.find(compliment) != m.end()){
+                return {m[compliment],i};
+            }
+            m[nums[i]] = i;
+        }
+        return {};
+        
+    }
+};
